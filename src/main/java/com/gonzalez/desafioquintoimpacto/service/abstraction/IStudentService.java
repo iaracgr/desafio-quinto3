ad805c03afdf2626eb5e91858ae2ad079b80939b
Camila Gonzalez <gcamila.iara@gmail.com>
2023-01-27 14:37:07 -0300

    added missing abstractions

--- src/main/java/com/gonzalez/desafioquintoimpacto/service/abstraction/IStudentService.java
+++ src/main/java/com/gonzalez/desafioquintoimpacto/service/abstraction/IStudentService.java
@@ -1,4 +1,26 @@
 package com.gonzalez.desafioquintoimpacto.service.abstraction;
 
+import com.gonzalez.desafioquintoimpacto.dto.ProfessorDto;
+import com.gonzalez.desafioquintoimpacto.dto.UserDto;
+
+import java.util.Date;
+import java.util.List;
+
 public interface IStudentService {
+
+    void create (UserDto dto);
+
+    void delete (String id) throws Exception;
+
+    UserDto updateUser(String id, String name, String email, String age, String history, Date birthday) throws Exception;
+
+    UserDto getByEmail (String email);
+
+    UserDto getById (String id);
+
+    List<UserDto> getAll();
+
+    UserDto addStudentToCourse(String idStudent, String idCourse) throws Exception;
+
+    UserDto removeStudentrFromCourse(String idStudent, String idCourse) throws Exception;
 }
