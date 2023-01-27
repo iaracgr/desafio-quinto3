c980cf993c0cea55655b63ee1cfeaa5f8993421b
Camila Gonzalez <gcamila.iara@gmail.com>
2023-01-27 12:18:32 -0300

    Initial commit

---
+++ src/main/java/com/gonzalez/desafioquintoimpacto/DesafioquintoimpactoApplication.java
@@ -0,0 +1,13 @@
+package com.gonzalez.desafioquintoimpacto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioquintoimpactoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioquintoimpactoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gonzalez/desafioquintoimpacto/DesafioquintoimpactoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalez.desafioquintoimpacto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioquintoimpactoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
